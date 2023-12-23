dcd05bd7f1bd2c80ca8198636533570b0ee6b0b7
bluhonglam <bluplam5@gmail.com>
2023-12-23 20:29:18 +0700

    first commit

---
+++ src/lam/dev/MainApp.java
@@ -0,0 +1,18 @@
+package lam.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+		
+		int number;
+		String nameSchool;
+		
+	}
+	
+	public void show() {
+		
+	}
+
+}
